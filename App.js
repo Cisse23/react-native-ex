ec2db3e9675e0e480556d7431f486603f5603710
Cisse Baker <baker.cisse@gmail.com>
2023-08-21 13:11:36 +0300

    eka palautus

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState("");
+
+  const buttonPressed = () => {
+    Alert.alert("Button pressed!");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text>Hello!</Text>
+        <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={text => setText(text)} 
+          value={text} 
+        />
+        <Button title="Click!!" onPress={buttonPressed}></Button>
+
+        <StatusBar style="auto" />
+      </View>
+      <View style={{flex: 2}}>
+
+      </View>
+
     </View>
   );
 }
@@ -15,6 +35,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    margin: 10
   },
 });
